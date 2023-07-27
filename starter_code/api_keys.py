3ad3ae910c7ac9f1ccf5c0b6b4faa1625bb65292
jeffdavidson343 <jeffreydavidson343@gmail.com>
2023-07-26 21:48:34 -0400

    hw update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "93e71f03eb39c82845e3d09af73a0e8a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0bd190a61d0a4076bf5064279a86ac78"
